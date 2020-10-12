4bc5dfb536118d5d7069e157bc4afc2caa197be3
IBUNHABIBU <ibunhabib@gmail.com>
2020-10-12 15:09:37 +0300

    correcting the lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": "plugin:react/recommended",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
@@ -11,6 +10,9 @@ module.exports = {
         "ecmaVersion": 12,
         "sourceType": "module"
     },
+    "plugins": [
+        "react"
+    ],
     "rules": {
     }
 };
