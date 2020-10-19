8790ae15fb91a36ec5dc3411a0eaa5790e396750
IBUNHABIBU <ibunhabib@gmail.com>
2020-10-19 13:20:24 +0300

    removing lint errors

--- src/modules/clear.js
+++
@@ -1,9 +0,0 @@
-const Clear = () => {
-  function clearPrevious(projectList) {
-    while (projectList.firstChild) {
-      projectList.removeChild(projectList.firstChild);
-    }
-  }
-  return { clearPrevious };
-};
-export default Clear;
